386739a7671f91de21934f682906f0d63ab3f836
Anders <anderslura@icloud.com>
2026-06-09 21:32:57 +0200

    Fjern DemoAnimasjon fra landingsside

--- src/components/Landingsside.jsx
+++ src/components/Landingsside.jsx
@@ -1,4 +1,3 @@
-import DemoAnimasjon from './DemoAnimasjon.jsx'
 function IkonRakett() {
   return (
     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -51,9 +50,6 @@ export default function Landingsside({ onStart }) {
         </button>
         <p className="landing-cta-hint">Ingen registrering. Ingen kredittkort.</p>
       </section>
-
-      {/* DEMO */}
-      <DemoAnimasjon />
 
       {/* FEATURES — Office 365-stil */}
       <section className="landing-features">
